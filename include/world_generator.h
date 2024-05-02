4cd7da726522b863f9a72732bc799558c69db073
TopeEstLa <contact.tope95@gmail.com>
2024-05-02 18:42:01 +0200

    add doc on world generator

--- include/world_generator.h
+++ include/world_generator.h
@@ -17,14 +17,44 @@ typedef enum {
     RIGHT = 3
 } Door_Face;
 
+/**
+ * Generate the first room and the rooms around it.
+ * @param world
+ */
 void base_generation(Game_World *world);
 
+/**
+ * Generate the rooms around the starting room.
+ * @param world
+ * @param starting_room
+ * @param recursion_depth
+ */
 void generate_rooms(Game_World *world, Room starting_room, int recursion_depth);
 
+/**
+ * Generate a room from a door of a starting_room.
+ * @param world
+ * @param starting_room
+ * @param door
+ * @param recursion_depth
+ */
 void generate_room(Game_World *world, Room starting_room, int door, int recursion_depth);
 
+/**
+ * Resize a room to avoid conflict with another room.
+ * @param room
+ * @param conflictRoom
+ * @param door_face
+ * @param door
+ * @return The resized room.
+ */
 Room resize_room(Room room, Room conflictRoom, int door_face, Pair* door);
 
+/**
+ * Check if a room is valid.
+ * @param room
+ * @return
+ */
 int is_room_valid(Room room);
 
 #endif //WORLD_GENERATOR_H
