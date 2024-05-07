d7c1bbcfe7026d6a53800af0cc62da783f533c48
paulmrll <160076071+paulmrll@users.noreply.github.com>
2024-05-07 11:41:41 +0200

    Update affichageProjet.c

--- src/affichageProjet.c
+++ src/affichageProjet.c
@@ -15,5 +15,6 @@ void afficheCam(char tab[LARGEUR][HAUTEUR], int x, int y, int l, int h){
             printf("%c", tab[i][j]);
         }
         printf("\n");
+        printf("sucemaqueueantonin\n);
     }
 }    
