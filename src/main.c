ec41088d64544ffea4e1e0afa6ca219c38c7bc8f
paulmrll <paul.morille11@gmail.com>
2024-05-10 16:15:53 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -23,11 +23,8 @@ int main() {
     add_entity(player, world->rooms[0].x + 3, world->rooms[0].y + 3);
 
 
-    initscr();
-    cbreak();
-    noecho();
 
-    curses_all_map(world);
+   print_all_map(world);
 
     while (1) {
 
