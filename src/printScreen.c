4791a0f6c86211bd94ca2153295e9f5f2b5e3f7f
paulmrll <paul.morille11@gmail.com>
2024-05-13 13:50:37 +0200

    Update printScreen.c

--- src/printScreen.c
+++ src/printScreen.c
@@ -11,24 +11,38 @@ void flushStdIn(){
 }
 
 WINDOW* printTimer(int* time){
+
+
     WINDOW * win = newwin(1, 12, 1, 1);
+
+
     while (*time >= 0){
         mvwprintw(win, 0, 0, "%3d", *time);
         wrefresh(win);
         *time = *time - 1;
         sleep(1);
     }
+
+
     return win;
 }
 WINDOW* printMap(char** tab, int x, int y, int dx, int dy, int height, int width){
+
+
     if (tab == NULL || x < 0 || x > width || y < 0 || y > height){
         exit(404);
     }
+
+
     WINDOW* win = newwin(2*dy, 2*dx, 1, 1);
+
+
     for (int i = y - dy; i <= y + dy; i++){
+
         if (tab[i] == NULL || i < 0 || i > height){
 
         } else {
+
             for (int j = x - dx; j <= x + dx; j++) {
                 if (j < 0 || j > width) {
 
@@ -36,20 +50,30 @@ WINDOW* printMap(char** tab, int x, int y, int dx, int dy, int height, int width
                     mvwprintw(win, i - (y - dy), j - (x - dx), "%c", tab[i][j]);
                 }
             }
+
             printw("\n");
         }
+
     }
+
+
     wrefresh(win);
     return win;
 }
 WINDOW* printInventary(){
+
+
     WINDOW* win = newwin(10, 2, 1, 1);
 
 
     return win;
 }
 int main() {
+
+
     initscr();
     printTimer();
     printMap(tab, 0, 0, 2, 2, 20, 20);
+
+    return 0;
 }
