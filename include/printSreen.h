77121e2ec7283130dd2eeb59dfb1edd6c5b49ea9
paulmrll <paul.morille11@gmail.com>
2024-05-14 15:24:16 +0200

    Create printSreen.h

---
+++ include/printSreen.h
@@ -0,0 +1,8 @@
+// Created by Paul Morille on 14/05/2024.
+//
+#include "world.h"
+#ifndef CYLETHOULECOMPANY_PRINTSREEN_H
+void printMap(Game_World* world, int x, int y, int dx, int dy);
+#define CYLETHOULECOMPANY_PRINTSREEN_H
+
+#endif //CYLETHOULECOMPANY_PRINTSREEN_H
