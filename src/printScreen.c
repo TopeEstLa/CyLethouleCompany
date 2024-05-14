723f2f173a79377933925cf9bbaa8ba1b27bfab4
paulmrll <paul.morille11@gmail.com>
2024-05-14 11:14:53 +0200

    Update printScreen.c

--- src/printScreen.c
+++ src/printScreen.c
@@ -3,14 +3,17 @@
 #include <unistd.h>
 #include <sys/time.h>
 
-
+//Print the timer on the terminal
 WINDOW* printTimer(struct timeval current) {
+
     start_color();
     WINDOW *win = newwin(1, 12, 1, 1);
     init_pair(1, COLOR_RED, COLOR_BLACK);
     struct timeval tv;
     gettimeofday(&tv, NULL);
+    //save the actual timeofday
     int difference = tv.tv_sec - current.tv_sec;
+    //Print in red if the difference is under 10 seconds
     if (difference <= 10){
         wattron(win, COLOR_PAIR(1));
         mvwprintw(win, 0, 0, "%3d", difference);
@@ -22,6 +25,7 @@ WINDOW* printTimer(struct timeval current) {
     return win;
 }
 
+//Print the map on the terminal
 void printMap(Game_World** tab, int x, int y, int dx, int dy, int height, int width){
     if (tab == NULL || x < 0 || x > width || y < 0 || y > height){
         exit(404);
@@ -35,9 +39,11 @@ void printMap(Game_World** tab, int x, int y, int dx, int dy, int height, int wi
                 if (j < 0 || j > width) {
 
                 } else {
+                    //print the item to stack or the emoji of the player
                     if (entity != NULL){
                         printw("  %s", entity->texture);
                     }else {
+                        //print the map : door, wall, etc ...
                         switch (tab->chunk[i][j]->type) {
                             case DOOR :
                                 printw("  D ");
