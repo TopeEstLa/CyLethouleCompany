7374a7a0b9024dfbd179be52a79675c1b1724f62
paulmrll <paul.morille11@gmail.com>
2024-05-14 23:13:42 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -25,7 +25,7 @@ int main() {
     add_entity(player, world->rooms[0].x + 3, world->rooms[0].y + 3);
 
 
-    printMap(world, world->rooms[0].x + 3, world->rooms[0].y + 3, 10, 10);
+    printMap(world, world->rooms[0].x + 3, world->rooms[0].y + 3, 20, 10);
 
     while (1) {
 
