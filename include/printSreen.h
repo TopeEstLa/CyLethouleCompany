78347f0c42349bc8e9f7c26c48c144292e4ee102
paulmrll <paul.morille11@gmail.com>
2024-05-14 23:24:54 +0200

    Update printSreen.h

--- include/printSreen.h
+++ include/printSreen.h
@@ -3,6 +3,8 @@
 #include "world.h"
 #ifndef CYLETHOULECOMPANY_PRINTSREEN_H
 void printMap(Game_World* world, int x, int y, int dx, int dy);
+void printName();
+void printTime();
 #define CYLETHOULECOMPANY_PRINTSREEN_H
 
 #endif //CYLETHOULECOMPANY_PRINTSREEN_H
