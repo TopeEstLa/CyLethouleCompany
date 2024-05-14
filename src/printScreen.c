0d9d8c110560aac0eaa10920e4d7cb64c76cfd2f
paulmrll <paul.morille11@gmail.com>
2024-05-14 10:22:35 +0200

    Update printScreen.c

--- src/printScreen.c
+++ src/printScreen.c
@@ -22,40 +22,46 @@ WINDOW* printTimer(struct timeval current) {
     return win;
 }
 
-WINDOW* printMap(char** tab, int x, int y, int dx, int dy, int height, int width){
-
-
+void printMap(Game_World** tab, int x, int y, int dx, int dy, int height, int width){
     if (tab == NULL || x < 0 || x > width || y < 0 || y > height){
-
         exit(404);
     }
-
-
-    WINDOW* win = newwin(2*dy, 2*dx, 1, 1);
-
-
     for (int i = y - dy; i <= y + dy; i++){
-
         if (tab[i] == NULL || i < 0 || i > height){
 
         } else {
-
             for (int j = x - dx; j <= x + dx; j++) {
+                Entity *entity = get_entity(x, y);
                 if (j < 0 || j > width) {
 
                 } else {
-                    mvwprintw(win, i - (y - dy), j - (x - dx), "%c", tab[i][j]);
+                    if (entity != NULL){
+                        printw("  %s", entity->texture);
+                    }else {
+                        switch (tab->chunk[i][j]->type) {
+                            case DOOR :
+                                printw("  D ");
+                                break;
+                            case WALL :
+                                printw("  | ");
+                                break;
+                            case void :
+                                printw("    ");
+                                break;
+                            case EMPTY :
+                                printw("    ");
+                                break;
+                            case default :
+                                printw("  ? ",);
+                                break;
+                        }
+                    }
                 }
             }
-
             printw("\n");
         }
-
     }
-
-
-    wrefresh(win);
-    return win;
+    refresh();
 }
 WINDOW* printInventary(){
 
@@ -64,13 +70,4 @@ WINDOW* printInventary(){
 
 
     return win;
-}
-int main() {
-
-
-    initscr();
-    printTimer();
-    printMap(tab, 0, 0, 2, 2, 20, 20);
-
-    return 0;
 }
