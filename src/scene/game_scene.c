8c39131b4fc8d63e6eff95d17ce53a1ae1b47036
TopeEstLa <contact.tope95@gmail.com>
2024-05-19 20:06:40 +0200

    change cam size

--- src/scene/game_scene.c
+++ src/scene/game_scene.c
@@ -39,105 +39,6 @@ void handle_game_input() {
     }
 }
 
-/*void game_scene_curses() {
-    if (!is_game_loaded()) {
-        set_current_scene(MAIN_MENU);
-        return;
-    }
-
-    Game_Data *game = get_game_data();
-    Game_World *world = game->world;
-    Entity *player = game->player->entity;
-
-
-    int x = player->x;
-    int y = player->y;
-
-
-    int lignes, colonnes;
-    int dx = 70;
-    int dy = 20;
-    getmaxyx(stdscr, lignes, colonnes);
-
-
-    int colonnes_text = dx;
-    int colonnes_debut = (colonnes - colonnes_text) / 2;
-    int lignes_debut = 6;
-    for (int iy = y - dy; iy < y + dy - 1; iy++) {
-
-        if (iy >= 0 && iy < world->height) {
-
-            for (int ix = x - dx; ix < x + dx - 1; ix++) {
-                if (ix >= 0 && ix < world->width) {
-                    Room *room = get_room(world, ix, iy);
-
-                    if (room == NULL) {
-                        mvprintw(lignes_debut, colonnes_debut, " ");
-                      //  mvprintw(lignes_debut, colonnes_debut, "   ");
-                        colonnes_debut++;
-                        continue;
-                    }
-
-                    if (!room->is_visited) {
-                        mvprintw(lignes_debut, colonnes_debut, " ");
-                        //mvprintw(lignes_debut, colonnes_debut, "   ");
-                        colonnes_debut++;
-                        continue;
-                    }
-
-                    Entity *entity = get_entity(ix, iy);
-                    if (entity != NULL) {
-                        mvprintw(lignes_debut, colonnes_debut, "%s", entity->texture);
-                        colonnes_debut++;
-                    } else {
-                        switch (world->chunk[ix][iy]->type) {
-                            case DOOR :
-                                mvprintw(lignes_debut, colonnes_debut, "۩");
-                                colonnes_debut++;
-                                break;
-                            case WALL :
-                                mvprintw(lignes_debut, colonnes_debut, "॥");
-                                colonnes_debut++;
-                                break;
-                            case VOID :
-                                mvprintw(lignes_debut, colonnes_debut, " ");
-                                colonnes_debut++;
-                                break;
-                            case EMPTY :
-                                mvprintw(lignes_debut, colonnes_debut, " ");
-                                colonnes_debut++;
-                                break;
-                            default :
-                                mvprintw(lignes_debut, colonnes_debut, "?");
-                                colonnes_debut++;
-                                break;
-                        }
-                    }
-                }
-            }
-            lignes_debut++;
-            colonnes_debut = (colonnes - colonnes_text) / 2;
-        }
-        lignes_debut = 6;
-        }
-    lignes_debut = 6;
-    colonnes_debut = (colonnes - colonnes_text * 2) / 2;
-    for (int i = y - dy; i < y + dy; i++) {
-        for (int j = x - dx; j < x + dx; j++) {
-            if (j == x - dx || j == x + dx - 1) {
-                mvprintw(lignes_debut, colonnes_debut, "|");
-                colonnes_debut++;
-            } else if (i == y - dy || i == y + dy - 1) {
-                mvprintw(lignes_debut, colonnes_debut, "-");
-                colonnes_debut++;
-            } else {
-                colonnes_debut++;
-            }
-        }
-        colonnes_debut = (colonnes - colonnes_text * 2) / 2;
-        lignes_debut++;
-    }
-}*/
 void game_scene_curses() {
     if (!is_game_loaded()) {
         set_current_scene(MAIN_MENU);
@@ -152,8 +53,8 @@ void game_scene_curses() {
     int y = player->y;
 
     int lignes, colonnes;
-    int dx = 70;
-    int dy = 20;
+    int dx = 40;
+    int dy = 10;
     getmaxyx(stdscr, lignes, colonnes);
 
     int colonnes_text = dx * 2;
